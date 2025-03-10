8de5f31e3788e68a32c1acb8b20c32bb31d275a4
Sujalpanchal23 <sujalpanchal2310@gmail.com>
2025-03-10 11:00:32 +0530

    Add files via upload

---
+++ ExplicittypeCasting.py
@@ -0,0 +1,59 @@
+#example 1
+'''a = 10
+b = 5
+c = a/b
+print(c, type(c))
+
+result = int(c)
+print(result, type(result))
+
+
+print(int(10/5))
+
+print(int(input("Enter the value of a :"))+ int(input("Enter the value of b :")))
+print(float(input("Enter the value of a : ")) * int(input("Enter the value of b :")))
+
+'''
+a = 100
+b = float(a)
+print(b,type(b))
+
+
+a = 100
+b = bool(a)
+print(b, type(b))
+
+a = 0
+b = bool(a)
+print(b,type(b))
+
+a = 100
+print(str(a), type(str(a)))
+
+a = 100
+b = str(a)
+print(b, type(str(a)))
+
+
+a = 100
+b = complex(a)
+print(b, type(b))
+
+a = 0
+b = complex(a)
+print(b, type(b))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ understandingdatatypes.py.py
@@ -0,0 +1,56 @@
+#Basic data types :
+'''a = 10
+print(type(a))
+
+a1 = 11228193829920392
+print(type(a1))
+
+b = 45.88
+print(type(b))
+
+c = True
+print(type(c))
+
+d = False
+print(type(d))
+
+e = "Sujal"
+print(type(e))
+
+f = "A"
+print(type(f))
+
+g = 8+3j
+print(type(g))
+
+
+x = 90
+y = 15
+print(x/y)'''
+
+k = 22
+l = 221
+result = k/l
+print(result)
+
+a = int(input("Enter the value of a :"))
+print(a, type(a))
+b = int(input("Enter the value of b :"))
+print(b, type(b))
+
+c = a + b
+print(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
