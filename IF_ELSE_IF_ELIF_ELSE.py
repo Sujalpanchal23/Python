f18147136e176993607377b1a83204becf69150a
Sujalpanchal23 <sujalpanchal2310@gmail.com>
2025-03-12 09:00:13 +0530

    Add files via upload

---
+++ IF_ELSE_IF_ELIF_ELSE.py
@@ -0,0 +1,48 @@
+'''#IF STATEMENTS PROGRAMS
+#Taking input from user.
+age = float(input("Enter your age : "))
+if (age>=18):
+    print("You are eligible for vote.")
+print("END")
+
+#by directly applying value.
+age1 = 23
+if(age1>=18):
+    print("Your age is greater than 18.")
+print("END")
+
+#IF ELSE STATEMENTS PROGRAMS
+#Taking input from user.
+
+age = float(input("Enter your age : "))
+if age>=185:
+    print("You are eligible for vote.")
+else:
+    print("You are not eligible for voting.")
+'''
+
+#IF - ELIF - ELSE STATEMENTS
+#taking input from user
+
+num = float(input("Enter the value : "))
+
+if num>0:
+    print("POSITIVE")
+
+elif num<0:
+    print("NEGATIVE")
+else:
+    print("ZERO")
+    
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ LOOPING.py
@@ -0,0 +1,70 @@
+'''#FOR LOOP
+a  = "SUJAL"
+
+for i in a:
+    print(i)
+
+a = "SUJAL AMITBHAI PANCHAL"
+
+for char in a:
+    print(char, end = "")
+
+#range function in for loop
+for i in range(1,100):
+    print(i, end = " ")
+#output
+
+
+for i in range(5):
+ print(i, end = " ")
+#output
+#0 1 2 3 4
+
+for i in range(3,-8,-1):
+    print(i, end = " ")
+
+
+for i in range(1, 10, 2):
+    print(i, end = " ")
+
+#range with 3 parametres
+for i in range(-3,5):
+   print(i, end = " ")
+#output
+  # -3 -2 -1 0 1 2 3 4
+  
+for i in range(1, -1, -1):
+  print(i, end = " ")
+ #output
+ #1 0
+
+for i in range(3,-10,-4):
+  print(i, end = " ")
+#output
+    #3 -1 -5 -9
+
+for i in range(0,11,-2):
+ print(i, end = " ")
+#output
+#ERROR
+
+for i in range(-5):
+   print(i , end = " ")
+#output
+    #EMPTY
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
